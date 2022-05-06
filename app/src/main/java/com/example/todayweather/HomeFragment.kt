b017aefde71cc18d936bee4e3e1ca3fae7de7248
kieubaongoc <tt.ngoc.kieu@tomosia.com>
2022-05-06 09:17:03 +0700

    fix git cmt response

--- app/src/main/java/com/example/todayweather/HomeFragment.kt
+++ app/src/main/java/com/example/todayweather/HomeFragment.kt
@@ -5,17 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.todayweather.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentHomeBinding.inflate(inflater)
+        binding = FragmentHomeBinding.inflate(inflater)
         binding.txtStatusDescription.setOnClickListener {
-            view?.findNavController()?.navigate(R.id.action_homeFragment_to_sevenDaysFragment)
+            findNavController()?.navigate(R.id.action_homeFragment_to_sevenDaysFragment)
         }
 
         return binding.root
